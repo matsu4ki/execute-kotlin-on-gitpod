a36bef4467f5ff78f135f831c7c9acffe476833d
matsu4ki <AmatsukiUrato@gmail.com>
2020-05-14 09:04:38 +0000

    れどめ追加

---
+++ README.md
@@ -0,0 +1,11 @@
+# Exec-kotlin-on-GitPod
+
+## Info
+
+GitPod上でkotlinを実行できるように環境構築したリポジトリです。
+
+```kotlin
+run main
+```
+
+で`main.kt`の実行結果がoutputに表示されます。
--- main.kt
+++ main.kt
@@ -1,5 +1,9 @@
 fun main(args: Array<String>) {
+    // 参考
+    // https://qiita.com/SYABU555/items/7cc5909c119ec9ab0288#for-index
+
     // if
+    println("===========================")
     val list = listOf("a", "b", "c")
 
     if (-1 !in 0..list.lastIndex) {
@@ -10,12 +14,26 @@ fun main(args: Array<String>) {
     }
     
     // for
+    println("===========================")
     for (x in 1..10) {
         println("hello" + x)
     }
 
+    // index取りたいとき
+    println("===========================")
     val array = listOf("a", "b", "c")
     for ((index, value) in array.withIndex()) {
         println("the element at $index is $value")
     }
+
+    // 範囲指定の方法
+    println("===========================")
+    for (i in 1..100) { println(i) } // closed range: includes 100
+    println("---------------------------")
+    for (i in 1 until 100) { println(i) } // half-open range: does not include 100 
+    println("---------------------------")
+    for (x in 2..10 step 2) { println(x) }
+    println("---------------------------")
+    for (x in 10 downTo 1) { println(x) }
+    println("===========================")
 }
